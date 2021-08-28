129a5f66d1834c57c25591253b71704f7fb14608
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-08-28 18:07:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+// write the code to print the following output Name : mr.x Univ:Univeersity of Dhaka//
+
+#include<stdio.h>
+int main()
+{
+    printf("name\t:\tMr.X\n");//`'\n create a new line and \t create a tab
+    printf("Univ\t:\tUniversity of dhaka\n");
+    return 0;
+    
+}
